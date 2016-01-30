8fdb36e268aa6a331f7a04b63e32c220b0d5680a
Javier Verdugo <javier@macbook-pro-de-javier.local>
2016-01-30 15:40:15 -0600

    Subiendo Playground

---
+++ Juego_de_Memoria.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+// Tarea: Juego de Memoria
+
+import UIKit
+
+
+for var i in  0...100 {
+    
+    if i % 5 == 0 {
+        
+        print("\(i) Bingo")
+    }
+    
+    if i % 2 == 0 {
+        
+        print("\(i) Par")
+    } else {
+        
+        print("\(i) Impar")
+        
+    }
+    
+    
+    if (i >= 30 && i <= 40) {
+        
+        print("\(i) Viva Swift")
+    }
+    
+    print("\n")
+    
+}
